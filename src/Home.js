46043381ec528378758d2597ef483b2d3713cac6
Avokoda <dakotamum@gmail.com>
2021-04-15 19:58:19 -0600

    cleaned up css a little and added picture :)

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './home.css'
-
+import logo from './logo.png'
 class Home extends React.Component{
     constructor(props){
         super(props)
@@ -12,6 +12,7 @@ class Home extends React.Component{
         return(
             <div className="home">
                 <h1>Home Page</h1>
+                <img src= {logo} alt="low on oil" />
             </div>
         )}
 }
