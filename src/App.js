875fb928a687b06f174be4022106fe7eaaec46f9
Brennon Netz <bnetz@gmail.com>
2021-04-19 19:45:41 -0600

    Removing files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import NavBar from './NavBar.js';
-import Home from './Home.js'
-import Schedule from './Schedule.js'
-import Ratings from './Ratings.js'
 import Login from './Login.js'
 import Signup from './Signup.js'
 import Dashboard from './Dashboard'
@@ -13,49 +9,6 @@ import PrivateRoute, { privateRoute } from './PrivateRoute'
 import './app.css';
 
 class App extends React.Component{
-	constructor(props){
-		super(props)
-		this.toggleHome = this.toggleHome.bind(this)
-		this.toggleSchedule = this.toggleSchedule.bind(this)
-		this.toggleRatings = this.toggleRatings.bind(this)
-		this.toggleLogin = this.toggleLogin.bind(this)
-		this.state={
-			key: this.props.id,
-			toggleHome: true,
-			toggleSchedule: false,
-			toggleRatings: false,
-			toggleLogin: false
-		}
-	}
-
-	toggleHome(){
-		this.setState({toggleHome: true})
-		this.setState({toggleSchedule: false})
-		this.setState({toggleRatings: false})
-		this.setState({toggleLogin: false})
-	}
-
-	toggleSchedule(){
-		this.setState({toggleSchedule: true})
-		this.setState({toggleHome: false})
-		this.setState({toggleRatings: false})
-		this.setState({toggleLogin: false})
-	}
-
-	toggleRatings(){
-		this.setState({toggleRatings: true})
-		this.setState({toggleHome: false})
-		this.setState({toggleSchedule: false})
-		this.setState({toggleLogin: false})
-	}
-
-	toggleLogin(){
-		this.setState({toggleHome: false})
-		this.setState({toggleSchedule: false})
-		this.setState({toggleRatings: false})
-		this.setState({toggleLogin: true})
-	}
-
 	render(){
 		return(
 		<Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh"}}>
@@ -71,11 +24,6 @@ class App extends React.Component{
 				</Router>
 			</div>
 		</Container>
-			// <div className="app">
-			// 	<NavBar toggleHome = {this.toggleHome} toggleSchedule = {this.toggleSchedule} toggleRatings = {this.toggleRatings} toggleLogin = {this.toggleLogin}
-			// 	togSched={this.state.toggleSchedule} togRate={this.state.toggleRatings} togHome={this.state.toggleHome} togLog={this.state.toggleLogin}/>
-			// 	{this.state.toggleHome ? (<div><Home /></div>) : this.state.toggleSchedule ? (<div><Schedule /></div>) : this.state.toggleRatings ? (<div><Ratings /></div>) : this.state.toggleLogin ? (<div><Login /></div>) : null}
-			// </div>
 		)
 	}
 }
