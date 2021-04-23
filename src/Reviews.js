bd23c71771e318dc39a434f6570157ea0b44ba4d
Avokoda <dakotamum@gmail.com>
2021-04-22 21:49:28 -0600

    Added razzle dazzle to rating components. Also, reviews page passes props to rating components

--- src/Reviews.js
+++ src/Reviews.js
@@ -5,7 +5,9 @@ import './reviews.css'
 export default function Reviews() {
     return (
         <div className='reviews'>
-            <Rating />
+            Look at our great reviews!
+            <Rating username="unicorndog" stars="3" ratingtext="Wow what a terrible place to eat."/>
+            <Rating username="emdawg" stars="1" ratingtext="I'm disappointed in the lack of oil variety. I am strictly vegan and was startled that this company does not offer natural, organic peanut oil."/>
         </div>
     )
 }
