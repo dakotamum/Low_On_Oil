53c2684a7dda77fa99c90112557f7dbedd38093d
Avokoda <dakotamum@gmail.com>
2021-04-27 20:28:57 -0600

    the reviews page grabs reviews!

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {useEffect} from 'react'
 import Rating from './Rating'
 import firebase from './firebase/firebaseConfig'
 import './reviews.css'
@@ -8,6 +9,7 @@ export default function Reviews() {
     var [username, setUser] = useState('');
     var [stars, setStars] = useState('');
     var [reviewtext, setReview] = useState('');
+    var [reviewsList, setReviewsList] = useState()
 
     const handleUsernameOnChange = (e) => {
         setUser(e.target.value);
@@ -20,17 +22,29 @@ export default function Reviews() {
     };
 
     const createReview = () =>{
-        const todoRef = firebase.database().ref("Review")
-        const todo = {
+        const reviewRef = firebase.database().ref("Review")
+        const review = {
             username,
             stars,
             reviewtext
         }
-        todoRef.push(todo)
+        reviewRef.push(review)
         setUser('')
         setStars('')
         setReview('')
     };
+
+    useEffect(() => {
+        const reviewsRef = firebase.database().ref("Review")
+        reviewsRef.on('value', (snapshot)=>{
+            const reviews = snapshot.val()
+            const reviewsList = []
+            for (let id in reviews){
+                reviewsList.push(reviews[id])
+            }
+            setReviewsList(reviewsList)
+        });
+    }, []);
 
     return (
         <div>
@@ -38,6 +52,9 @@ export default function Reviews() {
                 Look at our great reviews!
                 <Rating username="unicorndog" stars="3" ratingtext="Wow what a terrible place to eat."/>
                 <Rating username="emdawg" stars="1" ratingtext="I'm disappointed in the lack of oil variety. I am strictly vegan and was startled that this company does not offer natural, organic peanut oil."/>
+                {reviewsList ? reviewsList.map((review)=>(
+                    <Rating username = {review.username} stars = {review.stars} ratingtext = {review.reviewtext}/>
+                )) : ''}
                 <div className="reviewsubmition">
                     Leave us a review!
                     <br></br>
