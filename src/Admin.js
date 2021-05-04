d263c96eacf08c9df40086d70be6e499aea32622
Connor Prinster <connor.prinster@gmail.com>
2021-05-03 21:35:03 -0600

    admin stuffs

--- src/Admin.js
+++ src/Admin.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useEffect} from 'react'
 import './admin.css';
+import firebase from './firebase/firebaseConfig'
 
 
 export default function Admin() {
+
+    var [adminsList, setAdminsList] = useState()
+    var [username, setUsername] = useState('');
+
+    const handleUsernameOnChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    useEffect(() => {
+        // const reviewsRef = firebase.database().ref("Admins")
+        // reviewsRef.on('value', (snapshot)=>{
+        //     const admins = snapshot.val()
+        //     const adminsList = []
+        //     for (let id in admins){
+        //         adminsList.push(admins[id])
+        //     }
+        //     setAdminsList(adminsList)
+        //     console.log("weiner")
+        // });
+    }, []);
+
+    const createAdmin = () => {
+        const adminRef = firebase.database().ref("Admins")
+        const admin = {"username": username}
+        adminRef.push(admin)
+
+        setUsername("")
+    };
+
     return (
         <div>
             <ul>
@@ -14,8 +45,11 @@ export default function Admin() {
                     <li>View Calendar</li>
                     <li>View User's Information</li>
                 </ul>
-                
             </ul>
+            <br></br>
+            <input type="text" onChange={handleUsernameOnChange} value={username}></input>
+            <br></br>
+            <button onClick={createAdmin}>Submit</button>
         </div>
     )
 }
