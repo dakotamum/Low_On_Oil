d1bafd8ff7239599a533fddd6c8609e453cfafa0
Avokoda <dakotamum@gmail.com>
2021-05-04 21:54:10 -0600

    Current month displays correctly

--- src/Calendar.js
+++ src/Calendar.js
@@ -6,19 +6,44 @@ class Calendar extends React.Component{
     constructor(props){
         var today = new Date();
         var monthNumber = today.getMonth();
+        var year = today.getFullYear();
         var monthNames = ["January", "February", "March", "April", "May", "June", "July", "September", "October", "November", "December"];
         var currentMonthName = monthNames[monthNumber];
-        
+
+        var firstDay = new Date(year, monthNumber, 1).getDay()
+        var daysInMonth = new Date(year, monthNumber + 1, 0).getDate()
+
         super(props)
         this.state={
-            currentMonth: currentMonthName
+            currentMonth: monthNames[monthNumber],
+            currentYear: year,
+            firstWeekDay: firstDay,
+            numberOfDays: daysInMonth
         }
+        this.getBlanks = this.getBlanks.bind(this)
+        this.getDays = this.getDays.bind(this)
+    }
+
+    getBlanks(){
+        var blanks = [];
+        for (var i = 0; i < this.state.firstWeekDay; i++) {
+            blanks.push(<div className='blanks' key = {0-i}></div>);
+        }
+        return blanks;
+    }
+
+    getDays(){
+        var days = [];
+        for (var i = 0; i < this.state.numberOfDays; i++) {
+            days.push(<div className='day' key = {i}>{i + 1}</div>);
+        }
+        return days;
     }
 
     render(){
         return(
             <div className="calendar-wrapper">
-                <div className="month">{this.state.currentMonth}</div>
+                <div className="month">{this.state.currentMonth} {this.state.currentYear}</div>
                 <div className="calendar">
                     <div className="weekday">S</div>
                     <div className="weekday">M</div>
@@ -27,37 +52,8 @@ class Calendar extends React.Component{
                     <div className="weekday">T</div>
                     <div className="weekday">F</div>
                     <div className="weekday">S</div>
-                    <div className="day">1</div>
-                    <div className="day">2</div>
-                    <div className="day">3</div>
-                    <div className="day">4</div>
-                    <div className="day">5</div>
-                    <div className="day">6</div>
-                    <div className="day">7</div>
-                    <div className="day">8</div>
-                    <div className="day">9</div>
-                    <div className="day">10</div>
-                    <div className="day">11</div>
-                    <div className="day">12</div>
-                    <div className="day">13</div>
-                    <div className="day">14</div>
-                    <div className="day">15</div>
-                    <div className="day">16</div>
-                    <div className="day">17</div>
-                    <div className="day">18</div>
-                    <div className="day">19</div>
-                    <div className="day">20</div>
-                    <div className="day">21</div>
-                    <div className="day">22</div>
-                    <div className="day">23</div>
-                    <div className="day">24</div>
-                    <div className="day">25</div>
-                    <div className="day">26</div>
-                    <div className="day">27</div>
-                    <div className="day">28</div>
-                    <div className="day">29</div>
-                    <div className="day">30</div>
-                    <div className="day">31</div>
+                    {this.getBlanks()}
+                    {this.getDays()}
                 </div>
             </div>
         )}
